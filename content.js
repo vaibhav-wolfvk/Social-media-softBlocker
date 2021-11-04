4994872e6a2da7e4825c26eab52eda05239bea3d
Vaibhav Kushwaha <vaibhavkushwaha11@gmail.com>
2021-11-05 01:24:39 +0530

    2nd commit

--- content.js
+++ content.js
@@ -1,13 +1,66 @@
 // find hostname (just the first string after any www/m)
-let url=window.location.hostname;
-let url_split=url.split(".");
-let domain=url_split[0];
-if(domain=="www" || domain=='m'){
-    domain=url_split[1];
+let url = window.location.hostname;
+let url_split = url.split(".");
+let domain = url_split[0];
+if (domain == "www" || domain == "m") {
+  domain = url_split[1];
 }
 
 // Message to background page to show the icon
 // and start the timer for this domain if not yet started
-chrome.runtime.sendMessage({todo: "showPageAction",domain: domain},function(response){
+chrome.runtime.sendMessage(
+  { todo: "showPageAction", domain: domain },
+  function (response) {
     console.log("Response received!");
-});
+  }
+);
+
+//image/text changer
+function changer(d) {
+  if (d === undefined) {
+    return;
+  }
+  d = Number(d);
+  let h = Math.floor(d / 60);
+  if (h > 0) {
+    let images = ["icon.png"];
+
+    const imgs = document.getElementsByTagName("img");
+    for (let i = 0; i < imgs.length; i++) {
+      imgs[i].src = images[0];
+      console.log(imgs[i].src);
+      imgs[i].alt = "you have wasted yet another hour. take a break";
+    }
+
+    const tags = ["h1", "h3", "h2", "p", "a", "span"];
+    for (let j = 0; j < tags.length; j++) {
+      const head = document.getElementsByTagName(tags[j]);
+      for (let i = 0; i < head.length; i++) {
+        head[i].innerText = "you have wasted yet another hour. take a break";
+      }
+    }
+  }
+}
+// function to get latest timers from storage
+function updateTime() {
+  chrome.storage.sync.get(
+    ["youtube", "facebook", "twitter", "instagram"],
+    function (timeSpent) {
+      changer(timeSpent.youtube);
+
+      changer(timeSpent.facebook);
+
+      changer(timeSpent.twitter);
+
+      changer(timeSpent.instagram);
+    }
+  );
+}
+
+// Update timers every 1/2 a minute
+// $(document).ready(function () {
+//   updateTime();
+//   setInterval(updateTime, 30000);
+// });
+setTimeout(updateTime, 30000);
+//chrome.alarms.onAlarm.addListener(updateTime);
--- popup.js
+++ popup.js
@@ -1,31 +1,50 @@
 // function to convert minutes into string describing hours and minutes
-function secondsToHms(d) {
-    if(d==undefined ){
-        return "less than a minute";
-    }
-    d = Number(d);
-    let h = Math.floor(d/60);
-    let m = Math.floor(d%60);
-    if(h==0 && m==0){
-        return "less than a minute";
-    }
-    let hDisplay=h>0 ? h+(h==1 ? " hour " : " hours ") : "";
-    let mDisplay=m>0 ? m+(m==1 ? " minute " : " minutes ") : "";
-    return hDisplay+mDisplay; 
+function secondsToHms(d, tab) {
+  if (d == undefined || tab == 0) {
+    return "no tabs open";
+  }
+  d = Number(d);
+  let h = Math.floor(d / 60);
+  let m = Math.floor(d % 60);
+  if (h == 0 && m == 0) {
+    return "less than a minute";
+  }
+  let hDisplay = h > 0 ? h + (h == 1 ? " hour " : " hours ") : "";
+  let mDisplay = m > 0 ? m + (m == 1 ? " minute " : " minutes ") : "";
+  return hDisplay + mDisplay;
 }
 
 // function to get latest timers from storage
-function updateTime(){
-    chrome.storage.sync.get(["youtube","facebook","twitter","instagram"],function(timeSpent){
-        $("#ydTime").html('<em>'+secondsToHms(timeSpent.youtube)+'</em>');
-        $("#fdTime").html('<em>'+secondsToHms(timeSpent.facebook)+'</em>');
-        $("#tdTime").html('<em>'+secondsToHms(timeSpent.twitter)+'</em>');
-        $("#idTime").html('<em>'+secondsToHms(timeSpent.instagram)+'</em>');
-    });
+function updateTime() {
+  chrome.storage.sync.get(
+    ["youtube", "facebook", "twitter", "instagram", "openTabs"],
+    function (timeSpent) {
+      $("#ydTime").html(
+        "<em>" +
+          secondsToHms(timeSpent.youtube, timeSpent.openTabs[3]) +
+          "</em>"
+      );
+      $("#fdTime").html(
+        "<em>" +
+          secondsToHms(timeSpent.facebook, timeSpent.openTabs[0]) +
+          "</em>"
+      );
+      $("#tdTime").html(
+        "<em>" +
+          secondsToHms(timeSpent.twitter, timeSpent.openTabs[2]) +
+          "</em>"
+      );
+      $("#idTime").html(
+        "<em>" +
+          secondsToHms(timeSpent.instagram, timeSpent.openTabs[1]) +
+          "</em>"
+      );
+    }
+  );
 }
 
-// Update timers every 1/2 a minute 
-$(document).ready(function(){
-    updateTime();
-    setInterval(updateTime,30000);
+// Update timers every 1/2 a minute
+$(document).ready(function () {
+  updateTime();
+  setInterval(updateTime, 30000);
 });
